0fdc71289c735e5bb58baa4849058b24dff9591a
jimbosl1ce <jdhogerty@gmail.com>
2021-09-14 12:30:20 -0700

    edits and mods from jimmy

--- src/Components/StockList/StockList.js
+++ src/Components/StockList/StockList.js
@@ -1,27 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import './StockList.css';
-import StockCard from '../StockCard/StockCard';
+import React, { useState, useEffect } from "react";
+import "./StockList.css";
+import StockCard from "../StockCard/StockCard";
 
-function StockList(props) {
+function StockList({ stocksOwned }) {
+  return (
+    <table className="StockList">
+      <thead>
+        <tr>
+          <th className="caption">Symbol</th>
+          <th className="caption">Equity</th>
+          <th className="caption">Dividends</th>
+          <th className="caption">Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {stocksOwned.length >= 1 &&
+          stocksOwned.map((stockObj) =>
+          <StockCard key={stockObj.symName} symbol={`${stockObj.symName}`} shares={`${stockObj.symNumber}`} avgPrice="25.00" />
+          )}
 
-    
-    return (
-        <table className="StockList">
-            <thead>
-                <tr> 
-                    <th className='caption'>Symbol</th>
-                    <th className='caption'>Equity</th>
-                    <th className='caption'>Dividends</th>
-                    <th className='caption'>Price</th>
-                </tr>
-            </thead>
-            <tbody>
-                <StockCard symbol="BXMT" shares="4" avgPrice="52.49" />
-                <StockCard symbol="SHOP" shares="10" avgPrice="120.12" />
-            </tbody>
-        </table>
-    )
-
+        {/* <StockCard symbol="BXMT" shares="4" avgPrice="52.49" />
+        <StockCard symbol="SHOP" shares="10" avgPrice="120.12" /> */}
+      </tbody>
+    </table>
+  );
 }
 
 export default StockList;
